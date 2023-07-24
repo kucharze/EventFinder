880058af72dda72540d99b1073f1061b58a316e4
Zachary Kuchar <zacharyk323@live.com>
2023-07-24 17:57:13 -0400

    Initial setup to search by performer

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -4,9 +4,9 @@ import { AppContext } from '../contexts/app_context'
 import axios from "axios";
 
 //https://api.seatgeek.com/2/venues?city=atlanta&client_id=MzUxMzQ3NDV8MTY5MDIxMDg3Ni4wMzM0NDY2&client_secret=54ff34499e1de30d55b12348090f73527deafbb637cc69edf831681d1df80b56
-
+//performaers?slug=${person}
 function Searchbar() {
-    const {state, setState} = useContext(AppContext)
+    const {state, setState, } = useContext(AppContext)
 
     const [search,setSearch] = useState('')
 
@@ -23,16 +23,19 @@ function Searchbar() {
 
     const handleSubmit = (e) =>{
         e.preventDefault();
-        console.log(state)
+        //console.log(state)
         getData(search)
     }
   return (
     <div className='search'>
       <h1>Find Upcoming events based on your location</h1>
+      <h1>Have different parameters based on </h1>
+      <button>Search by Location</button>
+      <button>Search by Performer</button>
       <form onSubmit={(e) => {handleSubmit(e)}}>
         <input type="text" value={search} 
             onChange={(e)=>{setSearch(e.target.value)}} 
-            placeholder='Find your next event...'
+            placeholder='Input City..'
         />
         <input type='submit'/>
       </form>
