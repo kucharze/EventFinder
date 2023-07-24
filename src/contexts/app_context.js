066ae625bf72969907ba8b5289e0779fcb98181d
Zachary Kuchar <zacharyk323@live.com>
2023-07-24 17:02:50 -0400

    Updated with performer value

--- src/contexts/app_context.js
+++ src/contexts/app_context.js
@@ -4,9 +4,10 @@ export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
   const [state, setState] = useState(null);
+  const [performer, setPerformer] = useState(false);
 
   return (
-    <AppContext.Provider value={{ state, setState }}>
+    <AppContext.Provider value={{ state, setState, performer, setPerformer }}>
       {props.children}
     </AppContext.Provider>
   );
