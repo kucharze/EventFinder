60e9f683d7802e33346c707aa5ca87a1f28d727f
Zachary Kuchar <zacharyk323@live.com>
2023-07-24 19:25:28 -0400

    Setup for logic for searching by performer

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -6,7 +6,7 @@ import axios from "axios";
 //https://api.seatgeek.com/2/venues?city=atlanta&client_id=MzUxMzQ3NDV8MTY5MDIxMDg3Ni4wMzM0NDY2&client_secret=54ff34499e1de30d55b12348090f73527deafbb637cc69edf831681d1df80b56
 //performaers?slug=${person}
 function Searchbar() {
-    const {state, setState, } = useContext(AppContext)
+    const {state, setState, setPerformer, performer} = useContext(AppContext)
 
     const [search,setSearch] = useState('')
 
@@ -21,22 +21,48 @@ function Searchbar() {
         setState(res.venues)
     };
 
+    const getPerson = async (item) => {
+        const dat = await axios.get(
+        `https://api.seatgeek.com/2/venues?city=${item}&client_id=MzUxMzQ3NDV8MTY5MDIxMDg3Ni4wMzM0NDY2&client_secret=54ff34499e1de30d55b12348090f73527deafbb637cc69edf831681d1df80b56`
+        );
+
+        const res = await dat.data;
+
+        console.log(res.venues);
+        setState(res.venues)
+    };
+
     const handleSubmit = (e) =>{
         e.preventDefault();
         //console.log(state)
-        getData(search)
+        if(performer){
+            console.log("Search term",search)
+        }
+        else{
+            getData(search)
+        }
+        
     }
   return (
     <div className='search'>
       <h1>Find Upcoming events based on your location</h1>
       <h1>Have different parameters based on </h1>
-      <button>Search by Location</button>
-      <button>Search by Performer</button>
+      <button onClick={()=>{setPerformer(false)}}>Search by Location</button>
+      <button onClick={()=>{setPerformer(true)}}>Search by Performer</button>
       <form onSubmit={(e) => {handleSubmit(e)}}>
-        <input type="text" value={search} 
+        {
+            !performer && <input type="text" value={search} 
             onChange={(e)=>{setSearch(e.target.value)}} 
             placeholder='Input City..'
         />
+        }
+        {
+            performer && <input type="text" value={search} 
+            onChange={(e)=>{setSearch(e.target.value)}} 
+            placeholder='Input Performer..'
+        />
+        }
+        
         <input type='submit'/>
       </form>
     </div>
