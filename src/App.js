bbbc97eea2b38d9dcc8239b48fb1b506503e92ac
Zachary Kuchar <zacharyk323@live.com>
2023-07-25 16:45:16 -0400

    Removed unneeded code

--- src/App.js
+++ src/App.js
@@ -5,21 +5,7 @@ import axios from "axios";
 import Searchbar from "./components/Searchbar";
 import SearchList from "./components/SearchList";
 
-//https://api.seatgeek.com/2/venues?city=atlanta&client_id=MzUxMzQ3NDV8MTY5MDIxMDg3Ni4wMzM0NDY2&client_secret=54ff34499e1de30d55b12348090f73527deafbb637cc69edf831681d1df80b56
 function App() {
-  const getData = async () => {
-    const dat = await axios.get(
-      "https://api.seatgeek.com/2/performers?slug=pittsburgh-pirates&client_id=MzUxMzQ3NDV8MTY5MDIxMDg3Ni4wMzM0NDY2&client_secret=54ff34499e1de30d55b12348090f73527deafbb637cc69edf831681d1df80b56"
-    );
-
-    const res = await dat.data;
-
-    console.log(res);
-  };
-
-  useEffect(() => {
-    //getData();
-  }, []);
   return (
     <div className="App">
       <h1>Events Finder</h1>
