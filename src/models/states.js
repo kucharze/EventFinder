70d940788a0a37832e75a608aa594b2769573384
Zachary Kuchar <zacharyk323@live.com>
2023-07-25 10:59:11 -0400

    update to state list

--- src/models/states.js
+++ src/models/states.js
@@ -6,6 +6,23 @@ const states = [
   { louisiana: "la" },
   { oklahoma: "ok" },
   { arizona: "az" },
+  { Maine: "me" },
+  { oregon: "or" },
+  { arkansas: "ar" },
+  { maryland: "md" },
+  { pennsylvania: "pa" },
+  { americansamoa: "as" },
+  { massachusetts: "ma" },
+  { puertorico: "pr" },
+  { california: "ca" },
+  { michigan: "mi" },
+  { rhodeisland: "ri" },
+  { colorado: "co" },
+  { minnesota: "mn" },
+  { southcarolina: "sc" },
+  { ohio: "oh" },
+  { ohio: "oh" },
+  { ohio: "oh" },
   { ohio: "oh" },
   { ohio: "oh" },
   { ohio: "oh" },
@@ -30,12 +47,7 @@ const states = [
 
 module.exports = states;
 
-/*Maine	ME	Oregon	OR
-Arkansas	AR	Maryland	MD	Pennsylvania	PA
-American Samoa	AS	Massachusetts	MA	Puerto Rico	PR
-California	CA	Michigan	MI	Rhode Island	RI
-Colorado	CO	Minnesota	MN	South Carolina	SC
-Connecticut	CT	Mississippi	MS	South Dakota	SD
+/*Connecticut	CT	Mississippi	MS	South Dakota	SD
 Delaware	DE	Missouri	MO	Tennessee	TN
 District of Columbia	DC	Montana	MT	Texas	TX
 Florida	FL	Nebraska	NE	Trust Territories	TT
