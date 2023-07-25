3724b82e3519977a964028d96eeca1f699f0e4b3
Zachary Kuchar <zacharyk323@live.com>
2023-07-25 10:03:06 -0400

    Added image

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import SearchList from "./components/SearchList";
 function App() {
   const getData = async () => {
     const dat = await axios.get(
-      "https://api.seatgeek.com/2/venues?city=atlanta&client_id=MzUxMzQ3NDV8MTY5MDIxMDg3Ni4wMzM0NDY2&client_secret=54ff34499e1de30d55b12348090f73527deafbb637cc69edf831681d1df80b56"
+      "https://api.seatgeek.com/2/performers?slug=pittsburgh-pirates&client_id=MzUxMzQ3NDV8MTY5MDIxMDg3Ni4wMzM0NDY2&client_secret=54ff34499e1de30d55b12348090f73527deafbb637cc69edf831681d1df80b56"
     );
 
     const res = await dat.data;
@@ -23,6 +23,11 @@ function App() {
   return (
     <div className="App">
       <h1>Events Finder</h1>
+      <img
+        src="https://cdn.pixabay.com/photo/2018/05/10/11/34/concert-3387324_640.jpg"
+        alt="crowd of people"
+        className="crowd"
+      />
       <Searchbar />
       <SearchList />
     </div>
